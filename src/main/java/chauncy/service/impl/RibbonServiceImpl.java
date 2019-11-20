c02a2220a321750e9148ec62ae17b9523d274577
WangTianci <chauncy__wang@163.com>
2019-11-20 11:17:49 +0800

    add Hystrix function at 2019-11-20 11:17

--- src/main/java/chauncy/service/impl/RibbonServiceImpl.java
+++ src/main/java/chauncy/service/impl/RibbonServiceImpl.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import chauncy.service.RibbonService;
 
 @Service
@@ -12,8 +14,12 @@ public class RibbonServiceImpl implements RibbonService{
 	@Autowired
 	RestTemplate restTemplate;
 	
+	@HystrixCommand(fallbackMethod="error")
 	public String indexLoad(String name) {
 		return restTemplate.getForObject("http://SPRINGCLOUD-EUREKACLIENT/client?name="+name, String.class);
 	}
-
+	
+	public String error(String name){
+		return "参数为："+name+"，调用SPRINGCLOUD-EUREKACLIENT失败！";
+	}
 }
